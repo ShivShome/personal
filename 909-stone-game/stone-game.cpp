b10f6181417c879b27a15586570e9093b8158d69
ShivShome <b24mt1032@iitj.ac.in>
2026-08-03 14:51:49 +0530

    Time: 7 ms (36.56%) | Memory: 19.7 MB (11.02%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>& dp, vector<int>& piles, int i, int j) {
+        if (i > j) {
+            return 0;
+        }
+
+        // Return memoized result if already calculated
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+
+        int left = piles[i]-solve(dp,piles,i+1,j);
+        int right = piles[j]-solve(dp,piles,i,j-1);
+
+        return dp[i][j] = max(left, right);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        if (n <= 1) return true;
+
+        return !(dp[0][0]);
     }
 };
